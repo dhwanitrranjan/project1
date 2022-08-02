79e57fc7607c82fab0092094c49ebc180f14aa94
dhwanitrranjan <109077000+dhwanitrranjan@users.noreply.github.com>
2022-08-02 17:25:35 +0530

    Login validation fixed

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 function Login() {
+  var email = useRef("")
+  var pw = useRef("")
 
   const navigate = useNavigate();
-  
-  const [form, setForm] = useState({
-    username:"",
-    password:""
-  })
 
   const [err, setErr] = useState({
     userNameErr:"",
@@ -20,41 +17,53 @@ function Login() {
   })
 
   let handleInput = (e) =>{
-    let name = e.target.name
-    let value = e.target.value
+    var userErr = false
+    var pwErr = false
+    e.preventDefault();
 
-    if (name === "password"){
-      if (value.length < 8 && value.length > 0){
-        setErr({...err, passwordErr:"The length of password should be more 8."})
-      } else if (!value.match(/^[ A-Za-z0-9_@./#&+-]*$/)) {
-        setErr({...err, passwordErr:"The password should contain alphanumeric characters along with special charecters like !, $, #, or %"})
-      } else {
-        setErr({...err, passwordErr:""})
-      }
+    if (email.current.value==="dhwanit@gmail.com") {
+      userErr = true
+    } else if (email.current.value.length < 3 && email.current.value.length > 15){
+      setErr({...err, userNameErr:"The length of username should be more than 3 and less than 15."})
+    } else if (email.current.value.length === 0) {
+      setErr({...err, userNameErr:"Can not be empty."})
+    } else {
+      setErr({...err, userNameErr:""})
+    }
+
+    if (pw.current.value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/)) {
+      pwErr = true
+    } else if (pw.current.value.length < 8){
+      setErr({...err, passwordErr:"The length of password should be more 8."})
+    } else if (pw.current.value.length === 0) {
+      setErr({...err, passwordErr:"Can not be empty."})
+    } else {
+      setErr({...err, passwordErr:"Should contain Uppercase, Lowercasenumbers and special charecters."})
+    }
+
+    console.log(userErr, pwErr)
+    if (userErr && pwErr){
+      navigate('/add-data')
     }
   }
 
   return (
     <div>
       <Container>
-        <Form style={{border: "100px solid"}} className="p-4">
+        <Form style={{border: "100px solid"}} className="p-4" onSubmit={handleInput}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" name="username" onChange={handleInput} required/>
-            <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-            </Form.Text>
+            <Form.Control type="email" placeholder="Enter email" name="username" ref={email}/>
           </Form.Group>
+          {err.userNameErr}
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" name="password" onChange={handleInput} required/>
+            <Form.Control type="password" placeholder="Password" name="password" ref={pw}/>
           </Form.Group>
           {err.passwordErr}
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit" onClick={() => navigate('add-data')}>Log In</Button>
+          <br/>
+          <Button variant="primary" type="submit">Log In</Button>
         </Form>
       </Container>
       <br />      
